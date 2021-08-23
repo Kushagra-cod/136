476e8dd7877f2c3014942b9fabdfb677df608fa3
Kushagra-cod <81215175+Kushagra-cod@users.noreply.github.com>
2021-08-23 19:03:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+objects=[];
+status="";
+
+function preload(){
+    video=createVideo('video.mp4');
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video.hide();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("model loaded!")
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
